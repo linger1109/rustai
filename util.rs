86537b3599275ac44000c899a685aba29d45e417
jordan8510s <107032504+jordan8510s@users.noreply.github.com>
2024-11-08 13:53:45 -0600

    Update util.rs
    Fixed most lines that were coming up as errors. Still have a few lines coming up as errors, they're marked in the comments with corresponding error messages.

--- util.rs
+++ util.rs
@@ -1,4 +1,5 @@
 use std::ops;
+use std::ops::{Index, IndexMut};
 
 #[derive(Default)]
 pub struct Vector {
@@ -19,23 +20,24 @@ impl Vector {
         }
     }
 
-    fn new_from_vec(fromVec: Vec<i32>) -> Self {
+    fn new_from_vec(fromVec: Vec<f64>) -> Self {
         Self {
             dimensions: fromVec.len(),
             values: fromVec
         }
     }
 
-    fn get_dimension() -> usize {
-        return dimensions;
+    fn get_dimension(&self) -> usize {
+        return self.dimensions;
     }
 
-    fn as_vec() -> Vec<f64> {
-        return values.clone(); // check if clone is necessary
+    fn as_vec(&self) -> Vec<f64> {
+        return self.values.clone(); // check if clone is necessary
     }
 }
 
 impl ops::Add for Vector {
+    type Output = Vector;
     fn add(self, rhs: Vector) -> Vector {
         assert_eq!(self.dimensions, rhs.dimensions);
         let mut v: Vec<f64> = Vec::new();
@@ -47,6 +49,7 @@ impl ops::Add for Vector {
 }
 
 impl ops::Sub for Vector {
+    type Output = Vector;
     fn sub(self, rhs: Vector) -> Vector {
         assert_eq!(self.dimensions, rhs.dimensions);
         let mut v: Vec<f64> = Vec::new();
@@ -58,10 +61,11 @@ impl ops::Sub for Vector {
 }
 
 impl ops::Mul for Vector {
+    type Output = f64;
     fn mul(self, rhs: Vector) -> f64 {
         assert_eq!(self.dimensions, rhs.dimensions);
-        let mut result: f64 = 0;
-        for i in 0..self.values.size() {
+        let mut result: f64 = 0.0;
+        for i in 0..self.values.len() {
             result += self[i] * rhs[i];
         }
         return result;
@@ -69,47 +73,47 @@ impl ops::Mul for Vector {
 }
 
 impl ops::Index<usize> for Vector {
-    fn index (self, i: usize) -> f64 {
-        self.values[i];
+    type Output = f64;
+    fn index (&self, i: usize) -> &f64 {
+        &self.values[i]
     }
 }
 
-impls ops::IndexMut<usize> for Vector -> &mut f64{
-    fn index (self, i: usize) -> f64 {
-        self.values[i];
+impl ops::IndexMut<usize> for Vector {
+    fn index_mut (&mut self, i: usize) -> &mut f64 {
+        &mut self.values[i]
     }
 }
 
-pub class Matrix {
+pub struct Matrix {
     width: usize,
     height: usize,
     values: Vec<Vector>
 }
 
 impl Matrix {
-    pub fn new(height: f64, width: f64) -> Self {
+    pub fn new(height: usize, width: usize) -> Self {
         // initialise values, defaults to zero V
-        let mut v: Vec<Vector> = vec![Vector::new_from_dims(width, 0); height];
+        let mut v: Vec<Vector> = vec![Vector::new_from_dims(0, width); height]; // ERROR "the trait `Clone` is not implemented for `Vector`"
         Self {width: width, height: height, values: v}
     }
 
-    pub fn new_from_vec(v: vec<Vector>) -> Self {
+    pub fn new_from_vec(v: Vec<Vector>) -> Self {
         // todo: check that the matrix is rectangular        
-        Self {
-            width: v[]
-        }
+        todo!();
+        // }
     }
 
-    pub fn getDimensions() -> (usize, usize) { (height, width) }
+    pub fn getDimensions(&self) -> (usize, usize) { (self.height, self.width) }
         
-    pub fn getWidth() -> usize { width }
+    pub fn getWidth(&self) -> usize { self.width }
     
-    pub fn getHeight() -> usize { height }
+    pub fn getHeight(&self) -> usize { self.height }
 
-    pub fn T() -> Matrix {
-        let mut res: Matrix = Matrix::new(width, height);
-        for i in 0..height {
-            for j in 0..width {
+    pub fn T(&self) -> Matrix {
+        let mut res: Matrix = Matrix::new(self.width, self.height);
+        for i in 0..self.height {
+            for j in 0..self.width {
                 res[j][i] = self[i][j];
             }
         }
@@ -119,25 +123,25 @@ impl Matrix {
 
 impl Index<usize> for Matrix {
     type Output = Vector;
-    fn index(&self, index: usize) -> &Self::Output {
-        self.values[index]
+    fn index(&self, index: usize) -> &Vector {
+        &self.values[index]
     }
 }
 
 impl IndexMut<usize> for Matrix {
-    type Output = Vector;
-    fn index(&self, index: usize) -> &mut Self::Output {
+    fn index_mut(&mut self, index: usize) -> &mut Vector {
         &mut self.values[index]
     }
 }
 
 
 impl ops::Add for Matrix {
+    type Output = Matrix;
     fn add(self, rhs: Matrix) -> Matrix {
         assert_eq!(self.width, rhs.width);
         assert_eq!(self.height, rhs.height);
 
-        let mut v: Vec<Vector> = vec![Vector::new_from_dims(self.width, 0); self.height];
+        let mut v: Vec<Vector> = vec![Vector::new_from_dims(self.width, 0); self.height]; // ERROR "the trait `Clone` is not implemented for `Vector`"
 
         for i in 0..self.height {
             for j in 0.. self.width {
@@ -151,11 +155,12 @@ impl ops::Add for Matrix {
 }
 
 impl ops::Sub for Matrix {
-    fn subtract(self, rhs: Matrix) -> Matrix {
+    type Output = Matrix;
+    fn sub(self, rhs: Matrix) -> Matrix {
         assert_eq!(self.width, rhs.width);
         assert_eq!(self.height, rhs.height);
 
-        let mut v: Vec<Vector> = vec![Vector::new_from_dims(self.width, 0); self.height];
+        let mut v: Vec<Vector> = vec![Vector::new_from_dims(self.width, 0.0); self.height]; // ERROR "the trait `Clone` is not implemented for `Vector`"
 
         for i in 0..self.height {
             for j in 0..self.width {
@@ -168,10 +173,11 @@ impl ops::Sub for Matrix {
     }
 }
 
-impl ops::Mult for Matrix {
-    pub fn multiply(self, rhs: Matrix) {
-        assert_eq!(self.width, self.height); 
-
+impl ops::Mul for Matrix {
+    type Output = Matrix;
+    fn mul(self, rhs: Matrix) -> Matrix {
+        // todo
+        todo!();
     }
 }
 
@@ -185,17 +191,20 @@ pub struct DataPoint {
     output: Option<Vector>,
 }
 impl DataPoint {
-    pub fn new(input_dimensions: usize, output_dimensions: usize) -> Self {
+    pub fn new_from_dims(input_dimensions: usize, output_dimensions: usize) -> Self {
         Self {
             input_dimensions,
             output_dimensions,
-            input: Vector::new(input_dimensions),
+            input: Vector::new_from_dims(input_dimensions, 0.0),
             output: None
         }
     }
-    pub fn with_vec(Vector& input, Option<Vector>& output) -> Self {
-        let input_dimensions: usize = input.len();
-        let output_dimensions: usize = output.unwrap().len();
+    pub fn with_vec(input: Vector, output: Option<Vector>) -> Self {
+        let input_dimensions: usize = input.get_dimension();
+        let output_dimensions: usize = match &output {
+            Some(vec) => vec.get_dimension(),
+            None => 0,
+        };
         Self {
             input_dimensions,
             output_dimensions,
