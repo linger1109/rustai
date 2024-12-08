9eb3dc90e56f21bbf0180ad314e47e667568953b
linger1109 <54038148+linger1109@users.noreply.github.com>
2024-12-08 16:19:12 -0600

    Models.rs, bugfix and bring over
    Brought over models.rs from the unsupervised branch and fixed many errors / bugs with the code.

--- models.rs
+++ models.rs
@@ -1,33 +1,46 @@
+use crate::util;
+
 pub struct KMeansClustering {
     dimensions: usize,
-    clusters: usize, // please rename this
-    cluster_centroids: Vec<Vector> // each element in the Vec is one of the centroids, and each centroid is described by a Vector (since it exists in n-dimensional space).
+    cluster_count: usize,
+    cluster_centroids: Vec<util::Vector>, // each element in the Vec is one of the centroids, and each centroid is described by a Vector (since it exists in n-dimensional space).
+}
 
-    const THRESHOLD_CHANGE = 0.001; //minimum (min change in centroids' position) required to stop training
-    pub fn fit(data: Vec<Vector>) {
-        assert!(clusters <= data.len());
+impl KMeansClustering {
+    const THRESHOLD_CHANGE: f64 = 0.001; // min change in centroids' position required to stop training
 
-        // clear any previous training, if any
-        cluster_centroids.clear();
+    pub fn new(dimensions: usize, cluster_count: usize) -> Self {
+        Self {
+            dimensions: dimensions,
+            cluster_count: cluster_count,
+            cluster_centroids: Vec::new() // defined empty for now, will be properly initialized in fit
+        }
+    }
+
+    pub fn fit(&mut self, data: Vec<util::Vector>) {
+        assert!(self.cluster_count <= data.len());
+
+        // clear (if any) previous training
+        self.cluster_centroids.clear();
 
         // not sure how to initialise centroids; just taking first n values for now. Doesn't work when some datas are equal.
-        for i in 0..clusters {
-            cluster_centroids.push(Vector.new_from_vec(data[i]));
+        for i in 0..self.cluster_count {
+            self.cluster_centroids.push(util::Vector::new_from_vec(data[i].as_vec()));
         }
-
-        let mut max_ds: f64 = 1000; // maximum change of distance of centroids from prev iteration
         
         // scratch spaces; defining here to avoid continuously allot'ing memory
         let mut allocated_clusters: Vec<usize> = vec![0; data.len()];
-        let mut cluster_datasum = Vec<Vector> = vec![Vector::new_from_dims(dimensions, 0); clusters]; // scratch space for computiung sum of centroids
-        let mut cluster_counter = Vec<usize> = vec![0; clusters]; // how many datapoints are in the ith cluster?
+        let mut cluster_datasum: Vec<util::Vector> = vec![util::Vector::new_from_dims(self.dimensions, 0.0); self.cluster_count]; // scratch space for computiung sum of centroids
+        let mut cluster_counter: Vec<usize> = vec![0; self.cluster_count]; // how many datapoints are in the ith cluster?
 
         loop {
             // no idea why rust doesn't have do whiles, but using this instead
 
+            let mut max_ds: f64 = 0.0; // maximum change of distance of centroids from prev iteration
+
             // wipe scratch space data (if it needs to be wiped)
-            for i in 0..clusters {
-                cluster_datasum[i] = Vector::new_from_dims(dimensions, 0);
+            for i in 0..self.cluster_count {
+                cluster_datasum[i] = util::Vector::new_from_dims(self.dimensions, 0.0);
                 cluster_counter[i] = 0;
             }
 
@@ -35,41 +48,71 @@ pub struct KMeansClustering {
             for i in 0..data.len() {
                 let mut closest: usize = 0;
                 let mut best_dist: f64 = f64::MAX;
-                for j in 0..clusters {
-                    let mut d: f64 = vector_distance(data[i], cluster_centroids[j]);
+                for j in 0..self.cluster_count {
+                    let d: f64 = util::vector_distance(&data[i], &self.cluster_centroids[j]);
                     if d < best_dist {
-                        closest = j; // #j is the cloest centroid
+                        closest = j; // #j is the closest centroid
                         best_dist = d;
                     }
                 }
-                allocated_clusters[i] = cloest;
+                allocated_clusters[i] = closest;
             }
 
             // for each centroid, find the average, i.e. new centroid.
             for i in 0..data.len() {
                 let cluster_id: usize = allocated_clusters[i];
                 cluster_counter[cluster_id] += 1;
-                cluster_datasum[cluster_id] += data[i];
+                cluster_datasum[cluster_id] = cluster_datasum[cluster_id].clone() + data[i].clone();
             }
 
             // divide to find the average
-            for i in 0..clusters {
+            for i in 0..self.cluster_count {
                 // better if we can divide Vector by a scalar, don't think that's a thing now
-                for j in 0..dimensions {
-                    cluster_datasum[i][j] /= cluster_counter[i];
+                for j in 0..self.dimensions {
+                    cluster_datasum[i][j] /= cluster_counter[i] as f64;
                 }
             }
 
             // copy values over, and compute the difference on the way
-            for i in 0..clusters {
-                let mut ds = vector_distance(cluster_centroids[i], cluster_datasum[i]);
-                max_ds = cmp::min(max_ds, ds);
-                cluster_centroids = cluster_datasum[i];
+            for i in 0..self.cluster_count {
+                let ds = util::vector_distance(&self.cluster_centroids[i], &cluster_datasum[i]);
+                if ds > max_ds {
+                    max_ds = ds;
+                }
+                self.cluster_centroids[i] = cluster_datasum[i].clone();
             }
 
-            if max_ds <= THRESHOLD_CHANGE {
+            //print!("{}", max_ds);
+            if max_ds <= Self::THRESHOLD_CHANGE {
                 break;
             }
         }
+    }
+
+    pub fn eval(&self, data: util::Vector) -> usize {
+        assert!(self.cluster_centroids.len() > 0);
+
+        let mut best: usize = 0;
+        let mut best_dist: f64 = util::vector_distance(&data, &self.cluster_centroids[0]);
+
+        for i in 1..self.cluster_count {
+            let this_dist: f64 = util::vector_distance(&data, &self.cluster_centroids[i]);
+            if this_dist < best_dist {
+                best_dist = this_dist;
+                best = i;
+            }
+        }
+
+        return best;
+    }
+
+    pub fn eval_batch(&self, data: Vec<util::Vector>) -> Vec<usize> {
+        let mut res: Vec<usize> = Vec::new();
+
+        for i in 0..data.len() {
+            res.push(self.eval(data[i].clone()));
+        }
+
+        return res;
     }
 }
